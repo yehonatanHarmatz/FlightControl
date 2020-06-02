838b907dabbd1487d5a2a5ce8177dda7382df549
noamyakov <59260056+noamyakov@users.noreply.github.com>
2020-06-02 23:51:36 +0300

    wwwroot folder

---
+++ wwwroot/Scripts/CommunicationWithServer.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+function deleteFlightFromServer(flightId) {
+    $.ajax({
+        url: 'api/Flights/' + flightId,
+        type: 'DELETE'
+    })
+}
+
+function getFlightPlanFromServer(flightId, callback) {
+    $.getJSON('api/FlightPlan/' + flightId, callback);
+}
+
+function getAllFlightsFromServer(callback) {
+    let d = new Date();
+    let s = d.toISOString().split('.')[0] + 'Z';
+    $.getJSON('api/Flights?relative_to=' + s + '&sync_all' , callback);
+}
