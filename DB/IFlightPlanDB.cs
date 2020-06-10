0ee6f6aca1f772dbc5e9b7ee1372d448a1fc531b
yehonatanHarmatz <47951976+yehonatanHarmatz@users.noreply.github.com>
2020-06-10 18:18:28 +0300

    Add files via upload

---
+++ DB/IFlightPlanDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightControlWeb.Models;
+namespace FlightControlWeb.DB
+{
+    public interface IFlightPlanDB
+    {
+        public Task<FlightPlan> LoadFP(string id);
+        public IAsyncEnumerable<FlightPlan> LoadAllFP();
+        public Task DeleteFlight(string id);
+        public Task<Boolean> IsExist(string id);
+        public Task SaveFP(FlightPlan fp);
+
+
+    }
+}
---
+++ DB/IFlightToServerDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.DB
+{
+    public interface IFlightToServerDB
+    {
+        public Task<string> LoadFlightServer(string id);
+        public Task SaveFlightToServer(string id, string serverID);
+        public Task<Boolean> IsFlightExternal(string id);
+        public Task DeleteFlightToServer(string flightId);
+        public Task<bool> IsFlightExist(string id);
+
+    }
+}
